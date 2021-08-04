b9713b64065450960e20e8b8bdff3cdcd0ce712c
José Monar <Jos Monar>
2021-08-04 14:57:05 -0500

    Added LoginScreen and Navbar designs.

---
+++ src/components/ui/Navbar.js
@@ -0,0 +1,11 @@
+export const Navbar = () => {
+    return (
+        <div className="navbar navbar-dark bg-dark mb-4">
+            <span className="navbar-brand">José</span>
+
+            <button className="btn btn-outline-danger">
+                <span>Salir</span>
+            </button>
+        </div>
+    );
+};
