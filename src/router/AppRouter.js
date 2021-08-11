02662d5520893cbaa90ca98c636474c8c526f05f
José Monar <Jos Monar>
2021-08-10 23:02:05 -0500

    Protecting routes.

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,28 +1,40 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect,
-} from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
 import { startChecking } from "../actions/auth";
 import { LoginScreen } from "../components/auth/LoginScreen";
 import { CalendarScreen } from "../components/calendar/CalendarScreen";
+import { PrivateRoute } from "./PrivateRouter";
+import { PublicRoute } from "./PublicRouter";
 
 export const AppRouter = () => {
     const dispatch = useDispatch();
+    const { checking, uid } = useSelector((state) => state.auth);
 
     useEffect(() => {
         dispatch(startChecking());
     }, [dispatch]);
 
+    if (checking) {
+        return <h5>Wait....</h5>;
+    }
+
     return (
         <Router>
             <div>
                 <Switch>
-                    <Route exact path="/login" component={LoginScreen} />
-                    <Route exact path="/" component={CalendarScreen} />
+                    <PublicRoute
+                        isAuthenticated={!!uid}
+                        exact
+                        path="/login"
+                        component={LoginScreen}
+                    />
+                    <PrivateRoute
+                        isAuthenticated={!!uid}
+                        exact
+                        path="/"
+                        component={CalendarScreen}
+                    />
 
                     <Redirect to="/" />
                 </Switch>
